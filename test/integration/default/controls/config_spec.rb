eb3c948199a0f8222f313a5caf683c0252c52f84
Daniel Dehennin <daniel.dehennin@ac-dijon.fr>
2019-07-31 15:43:44 +0200

    test(service): the service configuration file must exist
    * test/integration/default/controls/config_spec.rb: simple checks on
      the service configuration file.

--- test/integration/default/controls/config_spec.rb
+++ test/integration/default/controls/config_spec.rb
@@ -4,13 +4,26 @@ when 'arch', 'suse'
   config_filename = '/etc/dhcpd.conf'
 when 'debian'
   config_filename = '/etc/dhcp/dhcpd.conf'
+  service_config_filename = '/etc/default/isc-dhcp-server'
 when 'freebsd'
   config_filename = '/usr/local/etc/dhcpd.conf'
+  service_config_filename = '/etc/rc.conf.d/dhcpd'
 when 'redhat'
   config_filename = '/etc/dhcp/dhcpd.conf'
 end
 
 control 'DHCPD configuration' do
+  title 'should be generated properly'
+
+  describe file(config_filename) do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    its('mode') { should cmp '0644' }
+  end
+end
+
+control 'DHCPD service configuration' do
   title 'should be generated properly'
 
   describe file(config_filename) do
