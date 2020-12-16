487fd0b901f82e955fbb0554013420ad40713189
Imran Iqbal <iqbalmy@hotmail.com>
2020-12-16 06:29:23 +0000

    ci(gitlab-ci): use GitLab CI as Travis CI replacement
    * Automated using https://github.com/myii/ssf-formula/pull/275

--- release.config.js
+++ release.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   branch: 'master',
+  repositoryUrl: 'https://github.com/saltstack-formulas/dhcpd-formula',
   plugins: [
       ['@semantic-release/commit-analyzer', {
         preset: 'angular',
